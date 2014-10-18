abf4d9315c383e34f748edf7dc61e6134536b2ed
balassy <balassy@avalon.aut.bme.hu>
2014-10-18 06:05:15 +0200

    Removing the dummy password.

--- FosconSolution/Foscon.DesktopTester/MainForm.Designer.cs
+++ FosconSolution/Foscon.DesktopTester/MainForm.Designer.cs
@@ -103,7 +103,6 @@
 			this.txtPassword.PasswordChar = '*';
 			this.txtPassword.Size = new System.Drawing.Size(119, 20);
 			this.txtPassword.TabIndex = 4;
-			this.txtPassword.Text = "Kamer.Amera!";
 			// 
 			// lblPassword
 			// 
