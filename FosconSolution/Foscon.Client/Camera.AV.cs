ccf299d8ddced0ffc399db32eff78d4551802da7
balassy <balassy@avalon.aut.bme.hu>
2014-10-29 12:37:10 +0100

    Support for the getImageSetting operation added.

---
+++ FosconSolution/Foscon.Client/Camera.AV.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Foscon.Client
+{
+	public partial class Camera
+	{
+		/// <summary>
+		/// Returns the color attribute of the video.
+		/// </summary>
+		/// <remarks>Requires Visitor privileges.</remarks>
+		/// <returns>The previously set video settings.</returns>
+		public async Task<GetImageSettingResult> GetImageSettings()
+		{
+			return await this.Execute<GetImageSettingResult>( "getImageSetting" );
+		}
+	}
+}
---
+++ FosconSolution/Foscon.Client/Results/GetImageSettingResult.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Foscon.Client
+{
+	/// <summary>
+	/// Represents the result of the <c>GetImageSetting</c> operation.
+	/// </summary>
+	[XmlRoot( "CGI_Result" )]
+	public class GetImageSettingResult : ResultBase
+	{
+		/// <summary>
+		/// Gets or sets the previously set brightness level (0-100).
+		/// </summary>
+		[XmlElement("brightness")]
+		public byte Brightness { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the previously set contrast level (0-100).
+		/// </summary>
+		[XmlElement("contrast")]
+		public byte Contrast { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the previously set hue level (0-100).
+		/// </summary>
+		[XmlElement("hue")]
+		public byte Hue { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the previously set saturation level (0-100).
+		/// </summary>
+		[XmlElement("saturation")]
+		public byte Saturation { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the previously set sharpness level (0-100).
+		/// </summary>
+		[XmlElement("sharpness")]
+		public byte Sharpness { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the previously set denoise level.
+		/// </summary>
+		[XmlElement("denoiseLevel")]
+		public byte Denoise { get; set; }
+	}
+}
